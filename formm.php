368cb9a15b8202e1c22ab60fb8b3920c730badda
Ebukavy <133422903+Ebukavy@users.noreply.github.com>
2023-05-15 15:02:34 +0200

    Add files via upload

---
+++ formm.php
@@ -0,0 +1,9 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    echo "Ingevoerde gebruikersnaam: " . $username . "<br>";
+    echo "Ingevoerde wachtwoord: " . $password;
+}
+?>
